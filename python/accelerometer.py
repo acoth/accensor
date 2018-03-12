b37e237b5839853e893df252fed6d1f9cd643305
accensor <accensor@wayward-spleen.com>
2018-03-12 00:09:40 -0400

    added settable alarm functionality

--- python/accelerometer.py
+++ python/accelerometer.py
@@ -5,17 +5,20 @@ class XL:
     lib = ctypes.cdll.LoadLibrary('../c-code/libaccelerometer.so')
 
     def __init__(self):
-        self.WriteReg(29,4) #Tap Threshold of +/-0.25g
+        self.WriteReg(29,32) #Tap Threshold of 2g
         self.WriteReg(30,0) #No X offset adjust
         self.WriteReg(31,0) #No Y offset adjust
         self.WriteReg(32,0) #No Z offset adjust
         self.WriteReg(33,255) #160ms max time for a tap
+        self.WriteReg(34,128) #min required separation between taps
+        self.WriteReg(35,255) #max window for second tap
         self.WriteReg(42,7) #Enable all axes for tap
         self.WriteReg(44,10) #Set data rate to 100Hz
         self.WriteReg(45,8) #Turn on accelerometer
-        #WriteReg(46,64) #turn on single-tap interrupts
+        self.WriteReg(46,96) #turn on both tap interrupts
         self.WriteReg(47,0) #direct all interrupts to pin 1
         self.WriteReg(49,15) #Set data format to full resolution
+        self.ReadReg(48)
         self.dataLog = []
     
     def WriteReg(self,reg,val):
@@ -38,7 +41,6 @@ class XL:
         self.dataLog.append((t,a))
 
     def AmBeingShaken(self,threshold=2,duration=1,sampling=0.25):
-        self.ReadAccelerations()
         meanAccel = reduce(lambda x,y: (x[0]+y[1][0]/len(self.dataLog),x[1]+y[1][1]/len(self.dataLog),x[2]+y[1][2]/len(self.dataLog)),self.dataLog,(0.,0.,0.))
         varianceMag = map(lambda x: (x[0],sum([(x[1][n]-meanAccel[n])**2 for n in range(3)])),self.dataLog);
         tOverThresh = [x[0] for x in varianceMag if x[1]>threshold]
@@ -47,3 +49,10 @@ class XL:
             if len(timeSeps)==0 or min(timeSeps)>sampling/2.0:
                 return False
         return True
+
+    def Tap(self):
+        return (self.ReadReg(48)&64)>0
+
+    def DoubleTap(self):
+        return (self.ReadReg(48)&32)>0
+    
--- python/clock.py
+++ python/clock.py
@@ -1,28 +1,63 @@
 #! /usr/bin/python
-from datetime import datetime
+from datetime import datetime,timedelta
 import time
 import colorsys
 import math
 import display
 import accelerometer
+import math
 
-# Make the color the time is displayed in change smoothly
-# in a full cycle every 6 hours
-# Draw the background black and the colon pale gray
+def SetAlarm(alarm,xl):
+    if alarm['enabled']:
+        alarm['enabled'] = False
+        return
+    doneSetting = False
+    while (not doneSetting):
+        a = xl.ReadAccelerations()
+        doneSetting = abs(a[2])>0.5
+        if (abs(a[1])<0.1):
+            a[1] = 0
+        angle = math.atan(a[1]/a[0])
+        alarm['setTime'] += timedelta(seconds=480*angle**3)
+        
+        display.DrawTime(alarm['setTime'].hour,alarm['setTime'].minute,(0.,0.005,0))
+        time.sleep(0.25)
+    alarm['enabled'] = True
+
+
+
 def runClock(cycles):
+    alarm = {'enabled':False,
+             'goingOff':False,
+             'setTime':datetime.now()}
     xl = accelerometer.XL()
     n=-1
     brightness = 0.0015
     display.SetDotCorrection(1,1,1)
+    t=0
     while (n<cycles):
-        t = datetime.now().time()
+        prevT=t
+        t = datetime.now()
+        if alarm['enabled'] and t.time() > alarm['setTime'].time() and prevT.time() < alarm['setTime'].time():
+            alarm['goingOff'] = True
+        
         cycleHours = 24
         cycleFrac = ((t.hour+t.minute/60.0+t.second/3600.0)%cycleHours)/cycleHours
-        fgColor = colorsys.hsv_to_rgb(cycleFrac,1,brightness)
         fsec = t.second+t.microsecond/1.0e6
-        colonColor = (0,0,0)
-        if xl.AmBeingShaken(duration=3):
+        if alarm['goingOff'] and fsec%2<1:
+            fgColor = (1,1,1)
+        else:
+            fgColor = colorsys.hsv_to_rgb(cycleFrac,1,brightness)
+        colonColor = (0,0,0) if not alarm['enabled'] else (0.001,0.001,0.001)
+        if alarm['goingOff']:
+            if xl.Tap():
+                alarm['goingOff'] = False
+        else:
+            if xl.DoubleTap():
+                SetAlarm(alarm,xl)
+        if xl.AmBeingShaken(duration=1):
             display.DrawIPAddr(reps=1)
+        xl.ReadAccelerations()            
         display.DrawTime(t.hour,t.minute,fg=fgColor,colon=colonColor)
         time.sleep(1/60.0)
         if (cycles>0):
